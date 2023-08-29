01c48db8ac6ac1963cda814580c90ce3212f2c07
Tanner Rhines <tannerrhines@protonmail.com>
2023-08-28 16:02:53 -0800

    updated event listener

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,16 @@
+
+const cityInput = document.querySelector(".city-input");
+
 const searchButton = document.querySelector(".search-btn");
 
-const cityInput = document.querySelector(".search-btn");
+
 
 
 const getCityLocation = () => {
     const cityName = cityInput.value.trim(); 
     // remove any extra spaces when user enters city ^
 
-    if (cityName) return;
+    if (!cityName) return;
 
     console.log(cityName);
 }
