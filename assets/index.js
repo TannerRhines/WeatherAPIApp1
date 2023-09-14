67c0c4194e2da0b3aa93fe20388f7a49f9cc340e
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 21:30:39 -0800

    created getWeatherDetails const, improved fetching method

--- assets/index.js
+++ assets/index.js
@@ -6,18 +6,28 @@ const searchButton = document.querySelector(".search-btn");
 
 const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a" // api key from openweather api 
 
+
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = ``;
+}
+
 const getCityLocation = () => {
     const cityName = cityInput.value.trim(); 
     // remove any extra spaces when user enters city ^
 
     if (!cityName) return;
 
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
 
+
+    // enter city coordinates from API response
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        console.log(data)
+        if (!data.length) return alert(`no coordinates found for ${cityName}`);
+
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
     }).catch(() => {
-        alert("error occured when fetching coordinates");
+        
     });
 };
 
