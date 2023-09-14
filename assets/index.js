ad1e0e16f377b9a580dabcf65dc96f80850265ea
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 21:46:43 -0800

    fixed typo, cleaned up code

--- assets/index.js
+++ assets/index.js
@@ -1,43 +1,37 @@
-
 const cityInput = document.querySelector(".city-input");
-
 const searchButton = document.querySelector(".search-btn");
+const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a";  // API key from OpenWeather API
 
 
-const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a" // api key from openweather api 
+
 
 
 const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-
-    fetch(WEATHER_API_URL).then(res => res.json()).then(fata => {
-        console.log(data)
-    }).catch(() => {
-        alert ("error occured while fetching forecast")
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(WEATHER_API_URL)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data); 
     })
-}
-
-const getCityLocation = () => {
-    const cityName = cityInput.value.trim(); 
-    // remove any extra spaces when user enters city ^
-
-    if (!cityName) return;
-
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
-
-
-    // enter city coordinates from API response
-    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        if (!data.length) return alert(`no coordinates found for ${cityName}`);
-
-        const { name, lat, lon } = data[0];
-        getWeatherDetails(name, lat, lon);
-    }).catch(() => {
-        
+    .catch(() => {
+        alert("Error occurred while fetching forecast");
     });
 };
 
-
+const getCityLocation = () => {
+    const cityName = cityInput.value.trim(); // Remove any extra spaces when user enters city
+    if (!cityName) return;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`; // Use HTTPS
+    fetch(GEOCODING_API_URL)
+    .then(res => res.json())
+    .then(data => {
+        if (!data.length) return alert(`No coordinates found for ${cityName}`);
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+    })
+    .catch(() => {
+        alert("Error occurred while fetching coordinates");
+    });
+};
 
 searchButton.addEventListener("click", getCityLocation);
-
