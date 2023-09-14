ec27ee8710f5d04ac719d3d7c091387a7546d0d5
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 23:40:46 -0800

    cleaned up code,  sorted variables, added comments

--- assets/index.js
+++ assets/index.js
@@ -1,53 +1,53 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
-
 const locationButton = document.querySelector(".location-btn");
-const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a";  // API key from OpenWeather API
-
+const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a"; // API key from OpenWeather API
 
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
+// Function to create weather cards
 const createWeatherCard = (cityName, weatherItem, index) => {
-
     const readableDate = new Date(weatherItem.dt_txt).toLocaleDateString('en-US', {
-        weekday: 'long', 
-        year: 'numeric', 
-        month: 'long', 
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
         day: 'numeric'
     });
 
-    if(index === 0) { //html for main weather card
+    const temp = (weatherItem.main.temp - 273.15).toFixed(2);
+    const windSpeed = weatherItem.wind.speed;
+    const humidity = weatherItem.main.humidity;
+    const icon = weatherItem.weather[0].icon;
+    const description = weatherItem.weather[0].description;
+
+    if (index === 0) {
+        // HTML for main weather card
         return `
-        <div class="details">
-                        <h2> ${cityName} ${readableDate}</h2>
-                        <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)} °C </h4>
-                <h4>Wind: ${weatherItem.wind.speed} m/s</h4>
-                <h4>Humidity: ${weatherItem.main.humidity} %</h4>
-                    </div>
-                    <div class="icon">
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                    <h4> ${weatherItem.weather[0].description} </h4>
-                    </div>
-                    
-                    `;
-    } else { //html for five day forecast card
-
-        
-    
-        return `<li class="card">
+            <div class="details">
+                <h2>${cityName} ${readableDate}</h2>
+                <h4>Temperature: ${temp} °C</h4>
+                <h4>Wind: ${windSpeed} m/s</h4>
+                <h4>Humidity: ${humidity} %</h4>
+            </div>
+            <div class="icon">
+                <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon">
+                <h4>${description}</h4>
+            </div>`;
+    } else {
+        // HTML for five-day forecast card
+        return `
+            <li class="card">
                 <h3>${readableDate}</h3>
-                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} °C </h4>
-                <h4>Wind: ${weatherItem.wind.speed} m/s</h4>
-                <h4>Humidity: ${weatherItem.main.humidity} %</h4>
-                </li>`;
+                <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon">
+                <h4>Temp: ${temp} °C</h4>
+                <h4>Wind: ${windSpeed} m/s</h4>
+                <h4>Humidity: ${humidity} %</h4>
+            </li>`;
     }
+};
 
-    }
- 
-
-
+// Function to fetch weather details
 const getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
@@ -65,30 +65,31 @@ const getWeatherDetails = (cityName, lat, lon) => {
             }
         });
 
-        // clear previous weather data
-
+        // Clear previous weather data
         cityInput.value = "";
         currentWeatherDiv.innerHTML = "";
         weatherCardsDiv.innerHTML = "";
 
-
-        // create card and adds to the DOM
+        // Create card and add it to the DOM
         fiveDaysForecast.forEach((weatherItem, index) => {
+            const cardHTML = createWeatherCard(cityName, weatherItem, index);
             if (index === 0) {
-                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                currentWeatherDiv.insertAdjacentHTML("beforeend", cardHTML);
             } else {
-                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                weatherCardsDiv.insertAdjacentHTML("beforeend", cardHTML);
             }
-            
         });
-    }).catch(() => {
+    })
+    .catch(() => {
         alert("Error occurred while fetching forecast");
     });
 };
 
+// Function to fetch city location
 const getCityLocation = () => {
     const cityName = cityInput.value.trim();
     if (!cityName) return;
+
     const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
     fetch(GEOCODING_API_URL)
@@ -103,20 +104,16 @@ const getCityLocation = () => {
     });
 };
 
-
+// Function to get user coordinates
 const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
         position => {
             const { latitude, longitude } = position.coords;
             const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
-                
-                
 
-            // get city name and coordinates
             fetch(REVERSE_GEOCODING_URL)
             .then(res => res.json())
             .then(data => {
-                
                 const { name } = data[0];
                 getWeatherDetails(name, latitude, longitude);
             })
@@ -127,9 +124,8 @@ const getUserCoordinates = () => {
         error => {
             console.log(error);
         }
-    )
-}
+    );
+};
 
 searchButton.addEventListener("click", getCityLocation);
-
 locationButton.addEventListener("click", getUserCoordinates);
