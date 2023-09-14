09a8f13010239922297051162ff1651925022d74
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 22:56:39 -0800

    debugging errors

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,6 @@ const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a";  // API key from OpenWeather API
 
-
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
 const createWeatherCard = (weatherItem) => {
@@ -11,12 +10,12 @@ const createWeatherCard = (weatherItem) => {
             <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
             <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} °C </h4>
             <h4>Wind: ${weatherItem.wind.speed} m/s</h4>
-            <h4>Humidity: ${weatherItem.main.humidity}</h4>
+            <h4>Humidity: ${weatherItem.main.humidity} %</h4>
             </li>`;
 }
 
 const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL)
     .then(res => res.json())
@@ -24,32 +23,28 @@ const getWeatherDetails = (cityName, lat, lon) => {
         const uniqueForecastDays = new Set();
         const fiveDaysForecast = [];
 
-        // Populate the uniqueForecastDays Set and fiveDaysForecast array
         data.list.forEach(forecast => {
-            const forecastDate = new Date(forecast.dt_txt).getDate(); 
-
-            if (!uniqueForecastDays.has(forecastDate) && uniqueForecastDays.size < 6) {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.has(forecastDate) && uniqueForecastDays.size < 5) {
                 uniqueForecastDays.add(forecastDate);
                 fiveDaysForecast.push(forecast);
             }
         });
 
-        console.log(fiveDaysForecast);
-
         fiveDaysForecast.forEach(weatherItem => {
-            createWeatherCard(weatherItem);
-        })
+            weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
+        });
     })
     .catch(() => {
         alert("Error occurred while fetching forecast");
     });
 };
 
-
 const getCityLocation = () => {
-    const cityName = cityInput.value.trim(); // Remove any extra spaces when user enters city
+    const cityName = cityInput.value.trim();
     if (!cityName) return;
-    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`; // Use HTTPS
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
     fetch(GEOCODING_API_URL)
     .then(res => res.json())
     .then(data => {
