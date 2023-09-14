45cf7d126226af127eda2296deeaf5a6387ba0c1
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 21:19:08 -0800

    created const for API geocoding url and API key

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,7 @@ const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
 
-
+const API_KEY = "4fdabe75c801b0fdd1fbc854ba76e26a" // api key from openweather api 
 
 const getCityLocation = () => {
     const cityName = cityInput.value.trim(); 
@@ -12,6 +12,10 @@ const getCityLocation = () => {
 
     if (!cityName) return;
 
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}
+
+    
+}`;
     console.log(cityName);
 }
 
